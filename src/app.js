b233043b0d6001865670fe14ff2e478b3777f292
ravinsan <39220869+ravinsan@users.noreply.github.com>
2024-10-06 21:35:40 +0530

    new changes

--- src/app.js
+++ src/app.js
@@ -2,12 +2,19 @@ import express from "express";
 import admin from "./route/admin.route.js";
 import bodyParser from 'body-parser';
 import cookieParser from "cookie-parser";
+import cors from "cors"
 
 const app = express();
 
 app.use(express.json());
 app.use(bodyParser.json());
 app.use(cookieParser());
+
+//app.use(cors('*'));
+app.use(cors({
+    origin: 'http://localhost:3000', // Your frontend URL
+    credentials: true, // Allow cookies to be sent
+}));
 
 app.use('/api/v1', admin);
 
