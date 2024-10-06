4fc3049a17b66c4cd9ccd14b528dbc42e53d6f6a
ravinsan <39220869+ravinsan@users.noreply.github.com>
2024-10-06 23:30:48 +0530

    bugs fixed

--- src/middleware/AuthMiddleware.js
+++ src/middleware/AuthMiddleware.js
@@ -5,13 +5,15 @@ export const AuthMiddleware = async (req, res, next) =>{
     
      try{
         // console.log(req.cookies);
-        const cookie_token = req.cookies.token;                        // Cookie Token  
-        const header_token = req.headers.authorization.split(" ")[1]; // Header Token
+        const cookie_token = req.cookies?.token;                        // Cookie Token  
+        const header_token = req.headers.authorization?.split(" ")[1]; // Header Token
 
         const usedToken = cookie_token || header_token;
 
-        if(!usedToken){
-            return res.status(401).json({message: "Unauthorized"})
+        if (!usedToken) {
+          return res
+            .status(401)
+            .json({ message: "Unauthorized: No token provided" });
         }
 
         const decoded = jwt.verify(usedToken, process.env.JWT_SECRET);
@@ -24,6 +26,6 @@ export const AuthMiddleware = async (req, res, next) =>{
         if(err.name === "TokenExpiredError"){
             return res.status(401).json({message: "Not Authorized"});
         }
-        return res.status(401).json({message: "Unauthorized"})
+        return res.status(501).json({message: "Something is missing!"})
      }
 }
